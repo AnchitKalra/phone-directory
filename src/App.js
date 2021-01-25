d2a88c486c86ce24bf58b753991bac946905d8e7
Anchit Kalra <ak007kalra@gmail.com>
2021-01-25 13:01:47 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@
 
 function App() {
   return (
-    <div >
+    <div className = "header">
+    {/* <div >
      Phone Directory
+    </div>
+    <button>Add</button>
+    <div>
+    <span>Name</span><br/>
+    <span>Phone</span>
+    </div> */}
+    <label htmlFor = "name">Name:</label>
+    <input id = "name" type = "text" placeholder = "Type Here" defaultValue = "AK"></input>
     </div>
   );
 }
